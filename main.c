bf8542ce0f9951e4f5054762b30c97e123b5dd2c
FMRG-P1 <franciscomanuelrayasgeronimo@gmail.com>
2025-03-03 15:50:51 -0600

    Publicacion inicial
    Santiago

---
+++ main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<raylib.h>
+
+int main(){
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow(500, 500, "Hello World");
+
+    while(!WindowShouldClose()){
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawCircle(50, 50, 20, RED);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
