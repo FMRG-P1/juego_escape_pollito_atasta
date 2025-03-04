9c23f1d44df57ecc679fe936b8baae14e1c112ca
FMRG-P1 <franciscomanuelrayasgeronimo@gmail.com>
2025-03-04 16:49:04 -0600

    hey

--- main.c
+++ main.c
@@ -7,6 +7,7 @@ struct Player{
     Vector2 position;
     Vector2 velocity;
     float mass;
+    float radius;
 };
 
 int main(){
@@ -16,46 +17,74 @@ int main(){
     struct Player player1 = {
         .position = (Vector2){100,100},
         .velocity = (Vector2){0,0},
-        .mass = 50
+        .mass = 50,
+        .radius = 20
     };
 
-    Rectangle platform = (Rectangle){200, 400, 100, 50};
+    const Rectangle platforms[] =  {
+        (Rectangle){200, 400, 100, 50}
+    };
 
     while(!WindowShouldClose()){
-        const float FRAME_TIME = GetFrameTime();
-        bool is_on_ground = player1.position.y+20 > GetScreenHeight();
-
         if(IsKeyPressed(KEY_F5)){
             player1.position = (Vector2){100,100};
             player1.velocity = (Vector2){0,0};
         }
-        
+
+        const float FRAME_TIME = GetFrameTime();
         Vector2 aplied_acceleration;
+        bool is_on_ground = false;
+
+        const Vector2 collision_0deg = (Vector2){player1.position.x+player1.radius, player1.position.y};
+        const Vector2 collision_90deg = (Vector2){player1.position.x-player1.radius, player1.position.y};
+        const Vector2 collision_180deg = (Vector2){player1.position.x-player1.radius, player1.position.y};
+        const Vector2 collision_270deg = (Vector2){player1.position.x+player1.radius, player1.position.y};
+        
         aplied_acceleration = (Vector2){0,9.8*player1.mass*FRAME_TIME};
 
-        if(is_on_ground){
-            aplied_acceleration.y += 0;
+        if(player1.position.y+20 > GetScreenHeight()){
+            is_on_ground = true;
+            aplied_acceleration.y = 0;
             player1.velocity.y = 0;
+        }
 
-            if(IsKeyReleased(KEY_SPACE)){
-                aplied_acceleration.y += -300;
+        for(int i=0; i<(sizeof(platforms)/sizeof(Rectangle)); i++){
+            if(CheckCollisionPointRec(collision_0deg, platforms[i]) || CheckCollisionPointRec(collision_90deg, platforms[i])){
+                aplied_acceleration.x = 0;
+                player1.velocity.x = 0;
+                is_on_ground = true;
+            }
+
+            if(CheckCollisionPointRec(collision_180deg, platforms[i])){
+                aplied_acceleration.y = 0;
+                player1.velocity.y = 0;
+                //player1.position.y = platforms[i].y - player1.radius;
+                is_on_ground = true;
             }
         }
 
-        if(IsKeyDown(KEY_LEFT)){
-            aplied_acceleration.x += -200*FRAME_TIME;
+        if(is_on_ground){
+            if(IsKeyDown(KEY_SPACE)){ aplied_acceleration.y += -300; }
         }
-        if(IsKeyDown(KEY_RIGHT)){
-            aplied_acceleration.x += 200*FRAME_TIME;
-        }
+
+        if(IsKeyDown(KEY_LEFT)){ aplied_acceleration.x += -200*FRAME_TIME; }
+        if(IsKeyDown(KEY_RIGHT)){ aplied_acceleration.x += 200*FRAME_TIME; }
         
         player1.velocity = Vector2Add(player1.velocity, aplied_acceleration);
         player1.position = Vector2Add(player1.position, Vector2Scale(player1.velocity, FRAME_TIME));
 
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            DrawCircle(player1.position.x, player1.position.y, 20, RED);
-            DrawRectangle(200, 400, 100, 50, RED);
+
+            DrawCircle(player1.position.x, player1.position.y, player1.radius, RED);
+            DrawCircleV(collision_0deg, 2, MAROON);
+            DrawCircleV(collision_90deg, 2, MAROON);
+            DrawCircleV(collision_180deg, 2, MAROON);
+            DrawCircleV(collision_270deg, 2, MAROON);
+
+            for(int i=0; i<(sizeof(platforms)/sizeof(Rectangle)); i++){
+                DrawRectangleRec(platforms[i], BLACK);
+            }
         EndDrawing();
     }
 
