2a8fc3545f63b075d9e0fca3bc8912b564242210
FMRG-P1 <franciscomanuelrayasgeronimo@gmail.com>
2025-03-05 16:21:22 -0600

    Santiago: incluido modo libre

--- main.c
+++ main.c
@@ -17,7 +17,6 @@ struct Player{
 };
 
 int main(){
-    SetTargetFPS(12);
     InitWindow(500, 500, "Hello World");
     Image imagen_jose_jose = LoadImage("./textures/JoseJose.jpeg");
     Texture2D textura_jose_jose = LoadTextureFromImage(imagen_jose_jose);
@@ -30,6 +29,13 @@ int main(){
         .radius = 20
     };
 
+    Camera2D old_player_camera;
+    Camera2D camera;
+    camera.target = (Vector2){ player1.position.x, player1.position.y };
+    camera.offset = (Vector2){ GetScreenWidth()/2.0f, GetScreenHeight()/2.0f };
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
+
     const Rectangle platforms[] =  {
         (Rectangle){200, 400, 100, 50},
         (Rectangle){0, 400, 10, 100}
@@ -37,38 +43,50 @@ int main(){
 
     bool is_on_ground = false;
 
-    float acc = 0;
-    float TIME_DELTA_STEP = 0.01;
+    float frame_time_acumulator = 0;
+    const float TIME_DELTA_STEP = 0.01;
+    bool free_mode = false;
 
-#if 1
     while(!WindowShouldClose()){
-        acc += GetFrameTime();
+        frame_time_acumulator += GetFrameTime();
 
-        while(acc >= TIME_DELTA_STEP){
-            acc -= TIME_DELTA_STEP;
+        while(frame_time_acumulator >= TIME_DELTA_STEP && !free_mode){
+            frame_time_acumulator -= TIME_DELTA_STEP;
             bool is_touching_ground = false;
 
+            // Determina colisiones
             if(player1.position.y + player1.radius >= GetScreenHeight()){
                 is_touching_ground = true;
             }
 
+            // Aplicar fuerzas instantáneas
             if(is_touching_ground && !is_on_ground){
                 player1.acceleration.y = 0;
                 player1.velocity.y = 0;
+
+                player1.acceleration.x *= 0.5;
             }
             if(is_touching_ground){
                 if(IsKeyDown(KEY_SPACE)){
                     player1.velocity.y -= 5*METER_UNIT;
+                    player1.acceleration.x *= 1.5;
                     is_touching_ground = false;
                 }
             }
     
             is_on_ground = is_touching_ground;
 
-
+            // Aplicar aceleraciones
             Vector2 added_acceleration = {0,0};
             if(!is_touching_ground){
                 added_acceleration.y += 9.8*player1.mass;
+            } else {
+                const float friction_coefficient = 0.1;
+                if(player1.velocity.x < 1 || player1.velocity.x > 1){
+                    added_acceleration.x += -friction_coefficient*9.8*player1.acceleration.x;
+                }else{
+                    player1.acceleration.x = 0;
+                }
             }
 
             {
@@ -78,79 +96,59 @@ int main(){
                 if(IsKeyDown(KEY_RIGHT)){ added_acceleration.x += 2*METER_UNIT*movement_x_factor; }
             }
 
+            // Computar movimiento
             player1.acceleration = Vector2Add(player1.acceleration, Vector2Scale(added_acceleration, TIME_DELTA_STEP));
             player1.velocity = Vector2Add(player1.velocity, Vector2Scale(added_acceleration, TIME_DELTA_STEP));
             player1.position = Vector2Add(player1.position, Vector2Scale(player1.velocity, TIME_DELTA_STEP));
+
+            if(GetWorldToScreen2D(player1.position, camera).y > GetScreenHeight()+20){
+                camera.target.y += GetWorldToScreen2D(player1.position, camera).y+player1.radius -  GetScreenHeight()+10;
+            }
+            if(GetWorldToScreen2D(player1.position, camera).x < 0+20 && player1.velocity.x < 0){
+                camera.target.x += - 20;
+            }
+
+            if(GetWorldToScreen2D(player1.position, camera).x > GetScreenWidth()-20 && player1.velocity.x > 0){
+                camera.target.x += player1.radius + 20;
+            }
+    
+        }
+
+        if(free_mode){
+            if(IsMouseButtonDown(MOUSE_BUTTON_LEFT)){
+                Vector2 mouse_delta = GetMouseDelta();
+                camera.offset = Vector2Add(camera.offset, mouse_delta);
+            }
+
+            float mouse_wheel = GetMouseWheelMove();
+            camera.zoom += mouse_wheel/10;
+        }
+        if(IsKeyPressed(KEY_TAB)){
+            if(!free_mode){
+                old_player_camera = camera;
+            }
+            if(free_mode){
+                camera = old_player_camera;
+            }
+            free_mode = !free_mode;
         }
 
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            DrawText(TextFormat("Acceleration: (%.2f, %.2f)", player1.acceleration.x, player1.acceleration.y), 100, 100, 20, BLACK);
-            DrawLineEx((Vector2){100, 100}, (Vector2){100+METER_UNIT,100}, 5, RED);
-            DrawCircle(player1.position.x, player1.position.y, player1.radius, RED);
-        EndDrawing();
-    }
-#else
-    while(!WindowShouldClose()){
-        const float COMMON_GROUND_FRICTION_COEFFICIENT = 12;
-        const float MOVEMENT_JUMP_SENSIBILITY = 1500;
-        const float MOVEMENT_DISPLACEMENT_SENSIBILITY = 800;
-        const float FRAME_TIME = GetFrameTime();
-
-        bool is_touching_ground = false;
-
-        Vector2 added_acceleration = {0,0};
-
-        if(player1.position.y + player1.radius >= GetScreenHeight()){
-            is_touching_ground = true;
-        }
-
-        for(int i=0; i<(sizeof(platforms)/sizeof(Rectangle)); i++){
-            if(CheckCollisionCircleRec((Vector2){player1.position.x, player1.position.y}, player1.radius, platforms[i])){
-                is_touching_ground = true;
-            }
-        }
-
-        if(!is_touching_ground){
-            if(FRAME_TIME < 0.5){
-                added_acceleration.y += 9.8*player1.mass; // Gravity
-            }
-        }
-        if(is_touching_ground && !is_on_ground){
-            player1.acceleration.y = 0;
-            player1.velocity.y = 0;
-        }
-        if(is_touching_ground){
-            if(IsKeyPressed(KEY_SPACE)){
-                player1.acceleration.y += MOVEMENT_JUMP_SENSIBILITY/player1.mass;
-            }
-        }
-
-        is_on_ground = is_touching_ground;
-
-        if(IsKeyDown(KEY_LEFT)){ added_acceleration.x += -MOVEMENT_DISPLACEMENT_SENSIBILITY; }
-        if(IsKeyDown(KEY_RIGHT)){ added_acceleration.x += MOVEMENT_DISPLACEMENT_SENSIBILITY; }
-          
-
-        player1.acceleration = Vector2Add(player1.acceleration, Vector2Scale(added_acceleration, FRAME_TIME));
-        player1.velocity = Vector2Add(player1.velocity, Vector2Scale(added_acceleration, FRAME_TIME));
-        player1.position = Vector2Add(player1.position, Vector2Scale(player1.velocity, FRAME_TIME));
-
-        BeginDrawing();
-            ClearBackground(RAYWHITE);
-            DrawCircle(player1.position.x, player1.position.y, player1.radius, RED);
-            DrawTextureEx(textura_jose_jose, (Vector2){player1.position.x-player1.radius, player1.position.y-player1.radius}, 0, 0.01, RAYWHITE);
-            DrawLineV(player1.position, (Vector2){player1.position.x + player1.acceleration.x, player1.position.y + player1.acceleration.y}, BLUE);
-
-            DrawText(TextFormat("Acceleration (%.2f, %.2f)", player1.acceleration.x, player1.acceleration.y), 100, 100, 20, BLACK);
-            DrawText(TextFormat("Position (%.2f, %.2f)", player1.position.x, player1.position.y), 100, 125, 20, BLACK);
-
-            for(int i=0; i<(sizeof(platforms)/sizeof(Rectangle)); i++){
+            DrawText(TextFormat("%s (Presiona TAB)", free_mode ? "MODO LIBRE" : "MODO CAMARA NORMAL"), 100, 70, 20, BLACK);
+            DrawText(TextFormat("Posicion: (%.2f, %.2f)", player1.position.x, player1.position.y), 100, 85, 20, BLACK);
+            DrawText(TextFormat("Acceleracion: (%.2f, %.2f)", player1.acceleration.x, player1.acceleration.y), 100, 100, 20, BLACK);
+        BeginMode2D(camera);
+            
+            for(int i=0; i<sizeof(platforms)/sizeof(Rectangle); i++){
                 DrawRectangleRec(platforms[i], BLACK);
             }
+
+            DrawCircle(player1.position.x, player1.position.y, player1.radius, RED);
+        EndMode2D();
         EndDrawing();
     }
-#endif
+
 
     UnloadTexture(textura_jose_jose);
     UnloadImage(imagen_jose_jose);
