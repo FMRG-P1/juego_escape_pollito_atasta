18256eab83d4ece04910a75ac7df434f3435c4ca
FMRG-P1 <franciscomanuelrayasgeronimo@gmail.com>
2025-03-05 17:30:36 -0600

    Santiago: Primer sistema de colisiones
    Falta agregar function lerp() para bajos framerates

--- main.c
+++ main.c
@@ -16,13 +16,20 @@ struct Player{
     float radius;
 };
 
+const Rectangle platforms[] =  {
+    (Rectangle){200, -100, 100, 50},
+    (Rectangle){0, -100, 10, 100},
+    (Rectangle){0, -200, 50, 50}
+};
+
 int main(){
+    //SetTargetFPS(8);
     InitWindow(500, 500, "Hello World");
     Image imagen_jose_jose = LoadImage("./textures/JoseJose.jpeg");
     Texture2D textura_jose_jose = LoadTextureFromImage(imagen_jose_jose);
 
     struct Player player1 = {
-        .position = (Vector2){100,200},
+        .position = (Vector2){100,-200},
         .velocity = (Vector2){0,0},
         .acceleration = (Vector2){0,0},
         .mass = 50,
@@ -36,11 +43,6 @@ int main(){
     camera.rotation = 0.0f;
     camera.zoom = 1.0f;
 
-    const Rectangle platforms[] =  {
-        (Rectangle){200, 400, 100, 50},
-        (Rectangle){0, 400, 10, 100}
-    };
-
     bool is_on_ground = false;
 
     float frame_time_acumulator = 0;
@@ -50,13 +52,64 @@ int main(){
     while(!WindowShouldClose()){
         frame_time_acumulator += GetFrameTime();
 
+        const Rectangle bounding_box = (Rectangle){player1.position.x-player1.radius, player1.position.y-player1.radius, player1.radius*2, player1.radius*2};
+        const Vector2 collision_left_1 = (Vector2){player1.position.x-player1.radius, player1.position.y-(player1.radius*0.4)};
+        const Vector2 collision_left_2 = (Vector2){player1.position.x-player1.radius, player1.position.y+(player1.radius*0.4)};
+        const Vector2 collision_right_1 = (Vector2){player1.position.x+player1.radius, player1.position.y-(player1.radius*0.4)};
+        const Vector2 collision_right_2 = (Vector2){player1.position.x+player1.radius, player1.position.y+(player1.radius*0.4)};
+
+        const Vector2 collision_up_1 = (Vector2){player1.position.x-(player1.radius*0.4), player1.position.y-player1.radius};
+        const Vector2 collision_up_2 = (Vector2){player1.position.x+(player1.radius*0.4), player1.position.y-player1.radius};
+        const Vector2 collision_down_1 = (Vector2){player1.position.x-(player1.radius*0.4), player1.position.y+player1.radius};
+        const Vector2 collision_down_2 = (Vector2){player1.position.x+(player1.radius*0.4), player1.position.y+player1.radius};
+        const Vector2 collision_down_3 = (Vector2){player1.position.x, player1.position.y+player1.radius};
+
+        Vector2 prev_player_pos = player1.position;
         while(frame_time_acumulator >= TIME_DELTA_STEP && !free_mode){
             frame_time_acumulator -= TIME_DELTA_STEP;
             bool is_touching_ground = false;
+            bool is_touching_roof = false;
+            bool is_touching_left_wall = false;
+            bool is_touching_right_wall = false;
 
             // Determina colisiones
-            if(player1.position.y + player1.radius >= GetScreenHeight()){
+            if(player1.position.y + player1.radius >= 0){
                 is_touching_ground = true;
+            }
+
+            for(int i=0; i<sizeof(platforms)/sizeof(Rectangle); i++){
+                if(CheckCollisionPointRec(collision_down_1, platforms[i]) || CheckCollisionPointRec(collision_down_2, platforms[i]) || CheckCollisionPointRec(collision_down_3, platforms[i])){
+                    float up_lerp = 1;
+                    Vector2 lerped_player_pos = player1.position;
+
+                    /*while(up_lerp >= 0){
+                        lerped_player_pos = Vector2Lerp(prev_player_pos, player1.position, up_lerp);
+                        Vector2 lerped_collision_down_1 = (Vector2){lerped_player_pos.x-(player1.radius*0.4), lerped_player_pos.y+player1.radius};
+                        Vector2 lerped_collision_down_2 = (Vector2){lerped_player_pos.x+(player1.radius*0.4), lerped_collision_down_1.y+player1.radius};
+                        Vector2 lerped_collision_down_3 = (Vector2){lerped_player_pos.x, lerped_player_pos.y+player1.radius};
+
+                        if(!CheckCollisionPointRec(lerped_collision_down_1, platforms[i]) && !CheckCollisionPointRec(lerped_collision_down_2, platforms[i]) && !CheckCollisionPointRec(lerped_collision_down_3, platforms[i])){
+                            player1.position = lerped_player_pos;
+                            printf("DO LERPS\n");
+                        }
+                        
+                        up_lerp-=0.25;
+                    }*/
+
+                    is_touching_ground = true;
+                }
+
+                if(CheckCollisionPointRec(collision_up_1, platforms[i]) || CheckCollisionPointRec(collision_up_2, platforms[i])){
+                    is_touching_roof = true;
+                }
+
+                if(CheckCollisionPointRec(collision_left_1, platforms[i]) || CheckCollisionPointRec(collision_left_2, platforms[i])){
+                    is_touching_left_wall = true;
+                }
+
+                if(CheckCollisionPointRec(collision_right_1, platforms[i]) || CheckCollisionPointRec(collision_right_2, platforms[i])){
+                    is_touching_right_wall = true;
+                }
             }
 
             // Aplicar fuerzas instantáneas
@@ -65,6 +118,18 @@ int main(){
                 player1.velocity.y = 0;
 
                 player1.acceleration.x *= 0.5;
+            }
+            if(is_touching_roof){
+                player1.acceleration.y = 0;
+                player1.velocity.y = 0;
+            }
+            if(is_touching_left_wall && player1.velocity.x < 0){
+                player1.acceleration.x = 0;
+                player1.velocity.x = 0;
+            }
+            if(is_touching_right_wall && player1.velocity.x > 0){
+                player1.acceleration.x = 0;
+                player1.velocity.x = 0;
             }
             if(is_touching_ground){
                 if(IsKeyDown(KEY_SPACE)){
@@ -97,10 +162,12 @@ int main(){
             }
 
             // Computar movimiento
+            prev_player_pos = player1.position;
             player1.acceleration = Vector2Add(player1.acceleration, Vector2Scale(added_acceleration, TIME_DELTA_STEP));
             player1.velocity = Vector2Add(player1.velocity, Vector2Scale(added_acceleration, TIME_DELTA_STEP));
             player1.position = Vector2Add(player1.position, Vector2Scale(player1.velocity, TIME_DELTA_STEP));
 
+            // TODO: ARREGLAR MOVIMIENTO CAMARA
             if(GetWorldToScreen2D(player1.position, camera).y > GetScreenHeight()+20){
                 camera.target.y += GetWorldToScreen2D(player1.position, camera).y+player1.radius -  GetScreenHeight()+10;
             }
@@ -135,17 +202,34 @@ int main(){
 
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            DrawText(TextFormat("%s (Presiona TAB)", free_mode ? "MODO LIBRE" : "MODO CAMARA NORMAL"), 100, 70, 20, BLACK);
-            DrawText(TextFormat("Posicion: (%.2f, %.2f)", player1.position.x, player1.position.y), 100, 85, 20, BLACK);
-            DrawText(TextFormat("Acceleracion: (%.2f, %.2f)", player1.acceleration.x, player1.acceleration.y), 100, 100, 20, BLACK);
+            DrawText(TextFormat("%s (Presiona TAB)", free_mode ? "MODO LIBRE" : "MODO CAMARA NORMAL"), 10, 70, 20, BLACK);
+            DrawText(TextFormat("Posicion: (%.2f, %.2f)", player1.position.x, player1.position.y), 10, 85, 20, BLACK);
+            DrawText(TextFormat("Acceleracion: (%.2f, %.2f)", player1.acceleration.x, player1.acceleration.y), 10, 100, 20, BLACK);
         BeginMode2D(camera);
+
+            DrawCircleLines(100,-200, 10, RED);
+            DrawText("Comienza aqui!!", 120, -200-5, 10, BLACK);
+            DrawLineV((Vector2){-1000, 0}, (Vector2){1000, 0}, BLACK);
+            DrawLineV((Vector2){0, -1000}, (Vector2){0, 1000}, BLACK);
+
             
             for(int i=0; i<sizeof(platforms)/sizeof(Rectangle); i++){
                 DrawRectangleRec(platforms[i], BLACK);
             }
-
+            
+            DrawRectangleLines(player1.position.x-player1.radius, player1.position.y-player1.radius, player1.radius*2, player1.radius*2, RED);
+            DrawCircleV(collision_left_1, 2, MAROON);
+            DrawCircleV(collision_left_2, 2, MAROON);
+            DrawCircleV(collision_right_1, 2, MAROON);
+            DrawCircleV(collision_right_2, 2, MAROON);
+            
+            DrawCircleV(collision_up_1, 2, DARKGREEN);
+            DrawCircleV(collision_up_2, 2, DARKGREEN);
+            DrawCircleV(collision_down_2, 2, DARKGREEN);
+            DrawCircleV(collision_down_1, 2, DARKGREEN);
+            DrawCircleV(collision_down_3, 2, DARKGREEN);
             DrawCircle(player1.position.x, player1.position.y, player1.radius, RED);
-        EndMode2D();
+            EndMode2D();
         EndDrawing();
     }
 
