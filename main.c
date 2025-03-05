67d21d094ff223b21c9413cc2ca7fe9317ed558a
FMRG-P1 <franciscomanuelrayasgeronimo@gmail.com>
2025-03-04 21:23:39 -0600

    Santiago

--- main.c
+++ main.c
@@ -4,10 +4,12 @@
 #include<raylib.h>
 #include<raymath.h>
 #include<math.h>
+#include<stdio.h>
 
 struct Player{
     Vector2 position;
     Vector2 velocity;
+    Vector2 acceleration;
     float mass;
     float radius;
 };
@@ -17,8 +19,9 @@ int main(){
     InitWindow(500, 500, "Hello World");
 
     struct Player player1 = {
-        .position = (Vector2){100,100},
-        .velocity = (Vector2){0,0},
+        .position = (Vector2){100,200},
+        .velocity = (Vector2){100,-50},
+        .acceleration = (Vector2){0,0},
         .mass = 50,
         .radius = 20
     };
@@ -29,9 +32,112 @@ int main(){
     };
 
     while(!WindowShouldClose()){
+<<<<<<< HEAD
+        Vector2 added_acceleration = {0,0};
+        bool is_on_ground = false;
+        bool is_on_wall = false;
+
+        if(player1.position.y + player1.radius >= GetScreenHeight()){
+            is_on_ground = true;
+        }
+
+        if(!is_on_ground){
+            added_acceleration.y += 9.8*player1.mass; // Gravity
+        } else {
+            player1.acceleration.y = 0;
+            player1.velocity.y = 0;
+
+            const float COMMON_GROUND_FRICTION_COEFFICIENT = 40;
+            const float MOVEMENT_JUMP_SENSIBILITY = 40;
+            if(player1.velocity.x > 0){
+                added_acceleration.x += -9.8*COMMON_GROUND_FRICTION_COEFFICIENT;
+            }
+            if(player1.velocity.x < 0){
+                added_acceleration.x += 9.8*COMMON_GROUND_FRICTION_COEFFICIENT;
+            }
+
+            if(IsKeyPressed(KEY_SPACE)){
+                added_acceleration.y = -MOVEMENT_JUMP_SENSIBILITY;
+            }
+        }
+
+        const float MOVEMENT_SENSIBILITY = 10;
+          
+
+        player1.acceleration = Vector2Add(player1.acceleration, Vector2Scale(added_acceleration, GetFrameTime()));
+        player1.velocity = Vector2Add(player1.velocity, Vector2Scale(added_acceleration, GetFrameTime()));
+        player1.position = Vector2Add(player1.position, Vector2Scale(player1.velocity, GetFrameTime()));
+=======
+        if(IsKeyPressed(KEY_F5)){
+            player1.position = (Vector2){100,100};
+            player1.velocity = (Vector2){0,0};
+        }
+
+        const float FRAME_TIME = GetFrameTime();
+        Vector2 aplied_acceleration;
+        bool is_on_ground = false;
+
+        const Vector2 collision_0deg = (Vector2){player1.position.x+player1.radius, player1.position.y};
+        const Vector2 collision_90deg = (Vector2){player1.position.x-player1.radius, player1.position.y};
+        const Vector2 collision_180deg = (Vector2){player1.position.x, player1.position.y-player1.radius};
+        const Vector2 collision_270deg = (Vector2){player1.position.x, player1.position.y+player1.radius};
+        
+        aplied_acceleration = (Vector2){0,9.8*player1.mass*FRAME_TIME};
+
+        if(player1.position.y+20 > GetScreenHeight()){
+            is_on_ground = true;
+            aplied_acceleration.y = 0;
+            player1.velocity.y = 0;
+        }
+
+        for(int i=0; i<(sizeof(platforms)/sizeof(Rectangle)); i++){
+            if(CheckCollisionPointRec(collision_90deg, platforms[i])){
+                aplied_acceleration.x = 0;
+                player1.velocity.x = 0;
+                player1.position.x += 1;
+                is_on_ground = true;
+            }
+
+            if(CheckCollisionPointRec(collision_0deg, platforms[i])){
+                aplied_acceleration.x = 0;
+                player1.velocity.x = 0;
+                player1.position.x += -1;
+                is_on_ground = true;
+            }
+
+            if(CheckCollisionPointRec(collision_180deg, platforms[i])){
+                player1.velocity.y = 0;
+                player1.position.y += 1;
+            }
+
+            if(CheckCollisionPointRec(collision_270deg, platforms[i])){
+                aplied_acceleration.y = 0;
+                player1.velocity.y = 0;
+                player1.position.y += -1;
+                is_on_ground = true;
+            }
+        }
+
+        if(is_on_ground){
+            if(IsKeyDown(KEY_SPACE)){ aplied_acceleration.y += -300; }
+        }
+
+        if(IsKeyDown(KEY_LEFT)){ aplied_acceleration.x += -200*FRAME_TIME; }
+        if(IsKeyDown(KEY_RIGHT)){ aplied_acceleration.x += 200*FRAME_TIME; }
+        
+        player1.velocity = Vector2Add(player1.velocity, aplied_acceleration);
+        player1.position = Vector2Add(player1.position, Vector2Scale(player1.velocity, FRAME_TIME));
+>>>>>>> parent of 8d43618 (Cambiado tasks.json SANTIAGO)
+
         BeginDrawing();
             ClearBackground(RAYWHITE);
+
             DrawCircle(player1.position.x, player1.position.y, player1.radius, RED);
+            DrawCircleV(collision_0deg, 2, MAROON);
+            DrawCircleV(collision_90deg, 2, MAROON);
+            DrawCircleV(collision_180deg, 2, MAROON);
+            DrawCircleV(collision_270deg, 2, MAROON);
+
             for(int i=0; i<(sizeof(platforms)/sizeof(Rectangle)); i++){
                 DrawRectangleRec(platforms[i], BLACK);
             }
